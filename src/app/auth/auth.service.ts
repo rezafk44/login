1d9684f5dec90192405c86ef00ec75e345a58e81
andrerusli <andre.rusli@umn.ac.id>
2019-11-05 09:46:25 +0700

    http client module

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -8,12 +8,17 @@ export class AuthService {
 
   constructor() { }
 
+  signup(email: string, password: string) {
+    //firebase signup API here
+  }
   login() {
+    //firebase login API here
     this.isAuthenticated = true;
     console.log(this.isAuthenticated);
   }
 
   logout() {
+    //firebase logout API here
     this.isAuthenticated = false;
     console.log(this.isAuthenticated);
   }
